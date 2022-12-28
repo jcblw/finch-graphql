d5ec8e97dcad410325c74cd7586a2747ce4b50ae
Jacob Lowe <jacoblowe2.0@gmail.com>
2022-12-28 10:51:55 -0600

    fix base path for documentation

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Finch GraphQL',
   tagline: 'Communication for modern extensions',
   url: 'https://jointoucan.github.io/finch-graphql/',
-  baseUrl: '/finch-graphql/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.svg',
